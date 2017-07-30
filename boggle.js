c02f5e0e105f8185b893c95933668e0a7d2994dc
Indraprahastha <indraprahastha@rocketmail.com>
2017-07-31 00:36:41 +0700

    Ampun-ga-nemu-caranya

--- boggle.js
+++ boggle.js
@@ -23,19 +23,53 @@ class boggleBoard {
   check(cek){
     var test = cek.split('')
     var tabel = [
-      [ 'D', 'T', 'Z', 'D', 'D', 'Z' ],
-      [ 'M', 'G', 'J', 'B', 'J', 'U' ],
-      [ 'F', 'D', 'I', 'V', 'S', 'E' ],
-      [ 'D', 'Q', 'M', 'R', 'H', 'F' ],
-      [ 'Z', 'G', 'D', 'R', 'K', 'O' ],
-      [ 'Z', 'T', 'C', 'S', 'O', 'G' ] ]
+      [ 0 , 0 ,  0 ,  0 ,  0 ,  0 ,  0 , 0 ]
+      [ 0 ,'D', 'T', 'Z', 'D', 'D', 'Z', 0 ],
+      [ 0 ,'M', 'G', 'J', 'B', 'J', 'U', 0 ],
+      [ 0 ,'F', 'D', 'I', 'V', 'S', 'E', 0 ],
+      [ 0 ,'D', 'Q', 'M', 'R', 'H', 'F', 0 ],
+      [ 0 ,'Z', 'G', 'D', 'R', 'K', 'O', 0 ],
+      [ 0 ,'Z', 'T', 'C', 'S', 'O', 'G', 0 ]
+      [ 0 , 0 ,  0 ,  0 ,  0 ,  0 ,  0 , 0 ]
+    ]
+    var nemu = []
 
-    for(i=0;i<test.length;i++){
-      for(j=0;j<tabel.length;j++){
-        for(k=0;k<tabel[j].length;k++)
+    for(var i=0;i<test.length;i++){
+      for(var j=0;j<tabel.length;j++){
+        for(var k=0;k<tabel[j].length;k++){
+
+          if(test[i]==tabel[j][k]){ //jika test ke i sama dengan abjad ke j - k
+            if(test[i+1]==tabel[j][k-1]){ //jika 1 huruf setelah test sama dengan abjad ke j sebelah kiri
+              nemu.push([test[i]]+'-> kiri '+tabel[j][k-1])
+            }
+            else if(test[i+1]==tabel[j][k+1]){ //jika 1 huruf setelah test sama dengan abjad ke j sebelah kanan
+              nemu.push([test[i]]+'-> kanan '+tabel[j][k+1])
+            }
+
+            else if(test[i+1]==tabel[j-1][k+1]){
+              nemu.push([test[i]]+'-> kanan atas '+tabel[j-1][k+1])
+            }
+            else if(test[i+1]==tabel[j-1][k-1]){
+              nemu.push([test[i]]+'-> kiri atas '+tabel[j-1][k-1])
+            }
+            else if(test[i+1]==tabel[j-1][k]){
+              nemu.push([test[i]]+'-> atas '+tabel[j-1][k])
+            }
+
+            else if(test[i+1]==tabel[j+1][k-1]){
+              nemu.push([test[i]]+'-> kiri bawah '+tabel[j+1][k-1])
+            }
+            else if(test[i+1]==tabel[j+1][k]){
+              nemu.push([test[i]]+'-> bawah '+tabel[j+1][k])
+            }
+            else if(test[i+1]==tabel[j+1][k+1]){
+              nemu.push([test[i]]+'-> kanan bawah '+tabel[j+1][k+1])
+            }
+          }
+        }
       }
     }
-    return test
+    return tabel+'\n'+nemu
   }
 
 
@@ -45,4 +79,5 @@ var rowCol = 6
 var abjadHuruf = new boggleBoard(abjad,rowCol)
 
 console.log(abjadHuruf.shake())
-console.log(abjadHuruf.check('ap'));
+console.log('<============ uji coba ==========>');
+console.log(abjadHuruf.check('QMRVJD'));
