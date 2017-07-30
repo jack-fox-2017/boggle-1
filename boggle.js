adedd4730cc444420978f41ffe25574b2d4cfc6b
Renataclara <renataclarak@gmail.com>
2017-07-30 23:10:18 +0700

    if set it back to static

--- boggle.js
+++ boggle.js
@@ -3,18 +3,24 @@
 const data = require('./data');
 // console.log(data.kamus[0]);
 var papan2 =
-  [ [ 'T', 'M', 'N', 'L' ],
-  [ 'R', 'S', 'U', 'O' ],
-  [ 'C', 'N', 'B', 'B' ],
-  [ 'Z', 'A', 'Z', 'T' ] ];
+[
+  ['D', 'G', 'H', 'I'],
+  ['K', 'L', 'P', 'S'],
+  ['Y', 'E', 'U', 'T'],
+  ['E', 'O', 'R', 'N']
+];
 
+// [ [ 'A', 'B', 'U', 'S' ],
+//   [ 'R', 'H', 'H', 'D' ],
+//   [ 'G', 'A', 'O', 'T' ],
+//   [ 'H', 'A', 'H', 'T' ] ];
 
 class BoggleBoard {
 
     constructor() {
       this.dictionary = data.kamus;
-      // this.papan = papan2;
-      this.papan = this.shake(4);
+      this.papan = papan2;
+      // this.papan = this.shake(4);
     }
 
     uniqueInput(n) {
@@ -88,140 +94,139 @@ class BoggleBoard {
       return true;
     }
 
+  
     checkPerSentence(arr) { // 's', 'i', 't'
-    // console.log('loo k sni');
-    var location = [];
-    let papan = this.papan;
-    // console.log(papan);
-    // console.log('LOLOLOLO');
-    var match = [];
-        for (let y=0; y<papan.length; y++) {
-          for (let x=0; x<papan[y].length; x++) {
-            let i = 0;
-            if (papan[y][x] == arr[i]) {
-              location.push([y,x]);
-              match.push(arr[i]);
-              // return location;
-              restartLoop:
-              while (i<papan.length) {
+        // console.log('loo k sni');
+        var location = [];
+        let papan = this.papan;
+        // console.log(papan);
+        // console.log('LOLOLOLO');
+        var match = [];
+            for (let y=0; y<papan.length; y++) {
+              for (let x=0; x<papan[y].length; x++) {
+                let i = 0;
+                if (papan[y][x] == arr[i]) {
+                  location.push([y,x]);
+                  match.push(arr[i]);
+                  // return location;
+                  restartLoop:
+                  while (i<papan.length) {
 
-                          if (i ==(papan.length-1) && arr.length == match.length) {
-                            // console.log('yess');
-                            return true;
-                          }
-                          if (x+1<papan.length) {
-                            if (papan[y][x+1] == arr[i+1]) {
-                              location.push([y,x+1]);
-                              match.push(arr[i+1]);
-                              y = y;
-                              x = x+1;
-                              i++;
-                              // console.log(i);
-                              // return location;
-                              continue restartLoop;
-                            }
-                          }
-                          if (x-1>0) {
-                              if (papan[y][x-1] == arr[i+1]) {
-                                location.push([y,x-1]);
-                                match.push(arr[i+1]);
-                                y = y;
-                                x = x-1;
-                                i++;
-                                continue restartLoop;
+                              if (i ==(papan.length-1) && arr.length == match.length) {
+                                // console.log('yess');
+                                return true;
                               }
-                          }
-                          if (y-1>0) { //major limit
-                              if (papan[y-1][x] == arr[i+1]) {
-                                location.push([y-1,x]);
-                                match.push(arr[i+1]);
-                                y = y-1;
-                                x = x;
-                                // console.log('p ny mana');
-                                i++;
-                                continue restartLoop;
-                              }
-                            if (x-1>0) {
-                              if (papan[y-1][x-1] == arr[i+1]) {
-                                location.push([y-1,x-1]);
-                                match.push(arr[i+1]);
-                                y = y-1;
-                                x = x-1;
-                                i++;
-                                continue restartLoop;
-                              }
-                            }
-                            if (x+1<papan.length) {
-                              if (papan[y-1][x+1] == arr[i+1]) {
-                                location.push([y-1,x+1]);
-                                match.push(arr[i+1]);
-                                y = y-1;
-                                x = x+1;
-                                i++;
-                                continue restartLoop;
-                              }
-                            }
-                          }
-                          if (y+1<papan.length) { //major limit
-                            // console.log('yeah');
-                              if (papan[y+1][x] == arr[i+1]) {
-                                location.push([y+1,x]);
-                                match.push(arr[i+1]);
-                                y = y+1;
-                                x = x;
-                                i++;
-                                // return location;
-                                continue restartLoop;
-                              }
-                            if (x-1>0 && y+1<papan.length) {
-                                if (papan[y+1][x-1] == arr[i+1]) {
-                                  location.push([y+1,x-1]);
+                              if (x+1<papan.length) {
+                                if (papan[y][x+1] == arr[i+1]) {
+                                  location.push([y,x+1]);
                                   match.push(arr[i+1]);
-                                  y = y+1;
-                                  x = x-1;
-                                  // return location;
+                                  y = y;
+                                  x = x+1;
                                   i++;
+                                  // console.log(i);
+                                  // return location;
                                   continue restartLoop;
-                                  // console.log(location);
-                                  // console.log('lwaet cini');
                                 }
                               }
-                            // console.log('lwaet cini');
-                            if (x+1<papan.length && y+1<papan.length) {
-                              if (papan[y+1][x+1] == arr[i+1]) {
-                                location.push([y+1,x+1]);
-                                match.push(arr[i+1]);
-                                y = y+1;
-                                x = x+1;
-                                i++;
-                                continue restartLoop;
+                              if (x-1>0) {
+                                  if (papan[y][x-1] == arr[i+1]) {
+                                    location.push([y,x-1]);
+                                    match.push(arr[i+1]);
+                                    y = y;
+                                    x = x-1;
+                                    i++;
+                                    continue restartLoop;
+                                  }
                               }
-                            }
-                          }
-                          // continue restartLoop;
+                              if (y-1>0) { //major limit
+                                  if (papan[y-1][x] == arr[i+1]) {
+                                    location.push([y-1,x]);
+                                    match.push(arr[i+1]);
+                                    y = y-1;
+                                    x = x;
+                                    // console.log('p ny mana');
+                                    i++;
+                                    continue restartLoop;
+                                  }
+                                if (x-1>0) {
+                                  if (papan[y-1][x-1] == arr[i+1]) {
+                                    location.push([y-1,x-1]);
+                                    match.push(arr[i+1]);
+                                    y = y-1;
+                                    x = x-1;
+                                    i++;
+                                    continue restartLoop;
+                                  }
+                                }
+                                if (x+1<papan.length) {
+                                  if (papan[y-1][x+1] == arr[i+1]) {
+                                    location.push([y-1,x+1]);
+                                    match.push(arr[i+1]);
+                                    y = y-1;
+                                    x = x+1;
+                                    i++;
+                                    continue restartLoop;
+                                  }
+                                }
+                              }
+                              if (y+1<papan.length) { //major limit
+                                // console.log('yeah');
+                                  if (papan[y+1][x] == arr[i+1]) {
+                                    location.push([y+1,x]);
+                                    match.push(arr[i+1]);
+                                    y = y+1;
+                                    x = x;
+                                    i++;
+                                    // return location;
+                                    continue restartLoop;
+                                  }
+                                if (x-1>0 && y+1<papan.length) {
+                                    if (papan[y+1][x-1] == arr[i+1]) {
+                                      location.push([y+1,x-1]);
+                                      match.push(arr[i+1]);
+                                      y = y+1;
+                                      x = x-1;
+                                      // return location;
+                                      i++;
+                                      continue restartLoop;
+                                      // console.log(location);
+                                      // console.log('lwaet cini');
+                                    }
+                                  }
+                                // console.log('lwaet cini');
+                                if (x+1<papan.length && y+1<papan.length) {
+                                  if (papan[y+1][x+1] == arr[i+1]) {
+                                    location.push([y+1,x+1]);
+                                    match.push(arr[i+1]);
+                                    y = y+1;
+                                    x = x+1;
+                                    i++;
+                                    continue restartLoop;
+                                  }
+                                }
+                              }
+                              // continue restartLoop;
 
-                          // console.log('sebelum sngaja muterin'+match);
-                          // console.log('sebelum sngaja muterin'+location);
-                            i++;
-                           continue restartLoop;
+                              // console.log('sebelum sngaja muterin'+match);
+                              // console.log('sebelum sngaja muterin'+location);
+                                i++;
+                               continue restartLoop;
+                }
+              }
             }
           }
+          if (arr.length == match.length) {
+            // console.log('yess end');
+            return true;
+          } else {return false;}
+          // return location;
         }
-      }
-      if (arr.length == match.length) {
-        // console.log('yess end');
-        return true;
-      } else {return false;}
-      // return location;
-    }
 
     finalCheck(arr) {
       // console.log(arr+'final');
-      if (this.checkWordAvailability(arr) == true) {
-        if (this.checkPerSentence(arr) == true) {
+      if (this.checkWordAvailability(arr) == true && this.checkPerSentence(arr) == true) {
           // console.log(arr);
           return true;
-        }
       } else {
         // console.log('bener kok');
       return false
--- data.js
+++ data.js
@@ -1,8 +1,9 @@
-var words = ["ABA","SUPER","ABAD","BA","BE","BU","CO","KU","KE","UN","MI","ID","CA",
-"DA","NULL","LOSE","GAIN","HOTEL","SLEEP","WALK","NIG","NA","NE","YE","QW","JI","RD",
-"NO","JORD","WA","EA","WIL","UNO","ZUS","LOVE","SOME","UO","POI"]
+// var words = ["ABA","SUPER","ABAD","BA","BE","BU","CO","KU","KE","UN","MI","ID","CA",
+// "DA","NULL","LOSE","GAIN","HOTEL","SLEEP","WALK","NIG","NA","NE","YE","QW","JI","RD",
+// "NO","JORD","WA","EA","WIL","UNO","ZUS","LOVE","SOME","UO","POI"]
 
-// var words = ["NUB","LOBT","TRSU"]
+// var words = [ "RHGA"];
+var words = ['APPLE', 'SIT', 'TRIP', 'TURN', 'SUPER', 'NRE'];
 
 
 
