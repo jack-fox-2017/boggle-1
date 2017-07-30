6b95d33058cf4a64ad1ed080d7528bae77b48f71
ahmadaidil <ahmdaidil@gmail.com>
2017-07-30 20:59:05 +0700

    boogle2 with skip lastPosition

--- boggle.js
+++ boggle.js
@@ -14,7 +14,7 @@ class Boogle {
     this.wordFound = [];
   }
 
-  printBoard(){
+  printBoard(){ //bisa juga dibilang shake krn me-random huruf yg akan diisi di board
     for(let i=0; i<this.large; i++){
       let row = [];
       for(let j=0; j<this.large; j++){
@@ -25,42 +25,52 @@ class Boogle {
     return this.arrBoard;
   }
 
-  nextPosition(word,index,lastPosition){
+  nextPosition(word,index,skipPos,lastPosition){
     let row = lastPosition[0];
     let col = lastPosition[1];
     let aroundPos = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
-    //let count = 0;
-    for(let j=0; j<aroundPos.length; j++){
-      if(this.arrBoard[row+aroundPos[j][0]]!=undefined && this.arrBoard[row+aroundPos[j][0]][col+aroundPos[j][1]]!=undefined && this.arrBoard[row+aroundPos[j][0]][col+aroundPos[j][1]]==word[index]){
-        return 1 + this.nextPosition(word, index+1, [row+aroundPos[j][0],col+aroundPos[j][1]]);
+    for(let j=aroundPos.length-1; j>0; j--){
+      let newRow = row+aroundPos[j][0];
+      let newCol = col+aroundPos[j][1];
+      if(this.arrBoard[newRow]!=undefined && this.arrBoard[newRow][newCol]!=undefined && this.arrBoard[newRow][newCol]==word[index]){
+        for(let s=0; s<skipPos.length; s++){
+          if(skipPos[s][0]!=newRow && skipPos[s][1]!=newCol){
+            skipPos.push(this.arrBoard[row]+aroundPos[j][0],this.arrBoard[row][col]+aroundPos[j][1]);
+            return 1 + this.nextPosition(word, index+1,skipPos,[newRow,newCol]);
+          }
+          else{
+            return 0;
+          }
+        }
       }
-      // else{
-      //   return 0 + this.nextPosition(word, index, [row,col]);
-      // }
     }
     return 0;
   }
 
   solve(){
     let lastPost = [0,0];
-    //let nextPost = [];
     for(let i=0; i<this.input.length; i++){
       for(let k=0; k<this.large; k++){
         for(let l=0; l<this.large; l++){
           if(this.arrBoard[k][l].indexOf(this.input[i][0])===0){
             lastPost[0] = k;
             lastPost[1] = l;
-            if(this.nextPosition(this.input[i],1,lastPost) == this.input[i].length-1){
+            if(this.nextPosition(this.input[i],1,[lastPost],lastPost) == this.input[i].length-1){
               this.wordFound.push(this.input[i]);
             }
           }
         }
       }
     }
-    return this.wordFound;
+    for(let w=0; w<this.wordFound.length; w++){
+      if(this.wordFound[w]==this.wordFound[w+1]){
+        this.wordFound.splice(w,1);
+      }
+    }
+    return this.wordFound.join(', ');
   }
 }
+
 let boo = new Boogle(5, arrData);
 console.log(boo.printBoard());
 console.log(boo.solve());
-//console.log(1+-2);
